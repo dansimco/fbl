677fb0c4a84f5d47d619f9c052b843f5b0283b68
Daniel Sim <daniel@explodingbox.com>
2012-02-06 22:15:39 +0000

    back to slow town

--- src/frontend/js.js
+++ src/frontend/js.js
@@ -700,29 +700,30 @@ window.addEvent('fbReady',function(){
   var view = renderView('main');
   view.inject(document.body);
 
-  FB.api('/me/home',function(response){
-    console.log(response);
-    response.data.each(function(post){
-      console.log(post);
-      if(typeof(post.from.category) == 'undefined'){
-        var p = new Post(post);
-        p.render().inject(view.getElement('.timeline'));
-      }
-    });
-  });
 
-  // FB.getLoginStatus(function(response) {
-  //   if(response.status = "connected") {
-  //     FB.api('/me',function(response){
-  //       console.log(response);
-  //     });
-  //   } else {
-  //     var auth_button = new Element('button.authenticate',{
-  //       'text':'Authenticate'
-  //     }).inject(view);
-  //   }
-  // });
-  // 
+
+  FB.getLoginStatus(function(response) {
+    FB.api('/me/home',function(response){
+      console.log(response);
+      response.data.each(function(post){
+        console.log(post);
+        if(typeof(post.from.category) == 'undefined'){
+          var p = new Post(post);
+          p.render().inject(view.getElement('.timeline'));
+        }
+      });
+    });
+    // if(response.status = "connected") {
+    //   FB.api('/me',function(response){
+    //     console.log(response);
+    //   });
+    // } else {
+    //   var auth_button = new Element('button.authenticate',{
+    //     'text':'Authenticate'
+    //   }).inject(view);
+    // }
+  });
+  
   // new Button(document.id('fb_auth'),{
   //   click: function(){
   //     FB.login(function(response) {
