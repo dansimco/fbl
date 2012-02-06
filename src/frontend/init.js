09ee90de2a78da3bc93f66461e0230df2a8a4bb7
Daniel Sim <daniel@explodingbox.com>
2012-02-06 22:19:32 +0000

    dont show photos

--- src/frontend/init.js
+++ src/frontend/init.js
@@ -1,44 +1,35 @@
 window.addEvent('fbReady',function(){
-  
   var view = renderView('main');
   view.inject(document.body);
-
-
-
   FB.getLoginStatus(function(response) {
-    FB.api('/me/home',function(response){
-      console.log(response);
-      response.data.each(function(post){
-        console.log(post);
-        if(typeof(post.from.category) == 'undefined'){
-          var p = new Post(post);
-          p.render().inject(view.getElement('.timeline'));
+    if(response.status = "connected") {
+      FB.api('/me/home',function(response){
+        console.log(response);
+        response.data.each(function(post){
+          console.log(post);
+          if(typeof(post.from.category) == 'undefined' && post.type != 'photo'){
+            var p = new Post(post);
+            p.render().inject(view.getElement('.timeline'));
+          }
+        });
+      });
+    } else {
+      var auth_button = new Element('button.authenticate',{
+        'text':'Authenticate'
+      }).inject(view);
+      new Button(document.id('fb_auth'),{
+        click: function(){
+          FB.login(function(response) {
+            if (response.authResponse) {
+              FB.api('/me',function(response){
+                console.log(response);
+              });
+            }
+          }, {scope: 'publish_stream,user_about_me,read_friendlists,read_stream,read_requests,publish_stream'});      
         }
       });
-    });
-    // if(response.status = "connected") {
-    //   FB.api('/me',function(response){
-    //     console.log(response);
-    //   });
-    // } else {
-    //   var auth_button = new Element('button.authenticate',{
-    //     'text':'Authenticate'
-    //   }).inject(view);
-    // }
-  });
-  
-  // new Button(document.id('fb_auth'),{
-  //   click: function(){
-  //     FB.login(function(response) {
-  //       if (response.authResponse) {
-  //         FB.api('/me',function(response){
-  //           console.log(response);
-  //         });
-  //       }
-  //     }, {scope: 'publish_stream,user_about_me,read_friendlists,read_stream,read_requests,publish_stream'});      
-  //   }
-  // });
-  
+    }
+  });  
 });
 
 
