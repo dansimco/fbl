0fa183e3959dad3567cec9856ab7cbf710232cf5
Daniel Sim <daniel@explodingbox.com>
2012-02-06 22:14:44 +0000

    test

--- src/frontend/init.js
+++ src/frontend/init.js
@@ -4,6 +4,7 @@ window.addEvent('fbReady',function(){
   view.inject(document.body);
 
   FB.api('/me/home',function(response){
+    console.log(response);
     response.data.each(function(post){
       console.log(post);
       if(typeof(post.from.category) == 'undefined'){
@@ -13,29 +14,29 @@ window.addEvent('fbReady',function(){
     });
   });
 
-  FB.getLoginStatus(function(response) {
-    if(response.status = "connected") {
-      FB.api('/me',function(response){
-        console.log(response);
-      });
-    } else {
-      var auth_button = new Element('button.authenticate',{
-        'text':'Authenticate'
-      }).inject(view);
-    }
-  });
-
-  new Button(document.id('fb_auth'),{
-    click: function(){
-      FB.login(function(response) {
-        if (response.authResponse) {
-          FB.api('/me',function(response){
-            console.log(response);
-          });
-        }
-      }, {scope: 'publish_stream,user_about_me,read_friendlists,read_stream,read_requests,publish_stream'});      
-    }
-  });
+  // FB.getLoginStatus(function(response) {
+  //   if(response.status = "connected") {
+  //     FB.api('/me',function(response){
+  //       console.log(response);
+  //     });
+  //   } else {
+  //     var auth_button = new Element('button.authenticate',{
+  //       'text':'Authenticate'
+  //     }).inject(view);
+  //   }
+  // });
+  // 
+  // new Button(document.id('fb_auth'),{
+  //   click: function(){
+  //     FB.login(function(response) {
+  //       if (response.authResponse) {
+  //         FB.api('/me',function(response){
+  //           console.log(response);
+  //         });
+  //       }
+  //     }, {scope: 'publish_stream,user_about_me,read_friendlists,read_stream,read_requests,publish_stream'});      
+  //   }
+  // });
   
 });
 
